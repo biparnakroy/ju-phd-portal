01e57e45418bb24d34f8b3a05e859a9b9cebe596
Biparnak Roy <roybiparnak@gmail.com>
2022-02-21 02:52:10 +0530

    fixing media storage

--- juphd/urls.py
+++ juphd/urls.py
@@ -22,4 +22,7 @@ from juphd import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('juphd_app.urls')),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+  urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
