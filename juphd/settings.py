d8a7c1c5b3e4e5b91e5572f45f2ede5100621d9a
Biparnak Roy <roybiparnak@gmail.com>
2022-02-20 23:40:51 +0530

    fixing settings

--- juphd/settings.py
+++ juphd/settings.py
@@ -170,6 +170,5 @@ AUTHENTICATION_BACKENDS = ['juphd_app.EmailBackEnd.EmailBackEnd']
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
