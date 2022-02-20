33f717483c244cd74c14c01fb1b27db5988a7f4e
Biparnak Roy <roybiparnak@gmail.com>
2022-02-20 23:51:06 +0530

    fixing password reset view

--- juphd_app/views.py
+++ juphd_app/views.py
@@ -62,4 +62,4 @@ class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
                       "if an account exists with the email you entered. You should receive them shortly." \
                       " If you don't receive an email, " \
                       "please make sure you've entered the address you registered with, and check your spam folder."
-    success_url = reverse_lazy('password_reset_done')
+    success_url = reverse_lazy('login')
