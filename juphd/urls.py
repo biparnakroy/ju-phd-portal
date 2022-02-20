463ed2dec75e37224911f6e47355c3292ee81f46
Biparnak Roy <roybiparnak@gmail.com>
2022-02-21 04:10:51 +0530

    fixing urls

--- juphd/urls.py
+++ juphd/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', include('juphd_app.urls')),
 ]
-if settings.DEBUG == None or settings.DEBUG == True:
+if settings.DEBUG == True:
   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
