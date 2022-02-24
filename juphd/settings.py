9366c14244a065d2528cac1c0e9def47533c28f2
Biparnak Roy <roybiparnak@gmail.com>
2022-02-25 02:36:58 +0530

    static files

--- juphd/settings.py
+++ juphd/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
-
+DEBUG = True
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','juphd.herokuapp.com']
 
 
