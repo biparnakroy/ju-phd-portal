7eef2162e3db2d8cbdfab559715ba7e554df92af
Biparnak Roy <roybiparnak@gmail.com>
2022-02-25 02:45:09 +0530

    fixing import error

--- juphd/urls.py
+++ juphd/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from juphd import settings , dev_settings
+from juphd import settings
 
 
 urlpatterns = [
